b41ad3bae724ed15f5d539b8f168b9fed16f0f48
Artur Gręziak <alex4448@wp.pl>
2020-02-05 23:49:03 +0100

    Variable

---
+++ 38.Define-a-variable-05-02-2020.js
@@ -0,0 +1,11 @@
+/*
+Define a variable
+Define a variable "count" inside the function defineVariable. Increment it and then return its value.
+*/
+function defineVariable() {
+    //define a variable "count" with value 0
+    let count = 0;
+    //then increment it
+    return count += 1;
+    //finally return it
+}
---
+++ 39.Variable-that-cannot-be-re-assigned-05-02-2020.js
@@ -0,0 +1,8 @@
+/*
+Variable that cannot be re-assigned
+Define a variable ageLimit that cannot be re-assigned and give it a value of 18.
+*/
+function defineVariable() {
+    //define a variable "ageLimit" with value 18 that cannot be re-assigned
+    const ageLimit = 18;
+}
