e087dbbc58103fe020adf734be19d1ce2643fd65
Artur Gręziak <alex4448@wp.pl>
2020-02-11 15:27:17 +0100

    Create 52.Map-string.js

---
+++ 52.Map-string.js
@@ -0,0 +1,12 @@
+// function wordLengths(arr) {
+//    return arr => arr.map(x => x.length);
+// }
+
+const wordLengths = arr => arr.map(x => x.length);
+
+const array1 = ["She", "sells", "seashells", "down", "by", "the", "seashore"];
+
+// pass a function to map
+const map1 = arr.map(x => x.length);
+
+console.log(map1);
