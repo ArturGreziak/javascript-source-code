c84af19a7325bf6e9701fbd394253f273371df8c
Artur Gręziak <alex4448@wp.pl>
2020-02-12 22:09:48 +0100

    Create 57.funkcja-binarna.js

---
+++ 57.funkcja-binarna.js
@@ -0,0 +1,8 @@
+// function binary(decimal) {
+//     return parseInt(decimal, 10).toString(2)
+// }
+
+const binary = decimal => parseInt(decimal, 10).toString(2); 
+
+console.log(binary(10));
+console.log(binary(5));
