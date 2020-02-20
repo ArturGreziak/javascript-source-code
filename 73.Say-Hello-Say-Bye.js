4bcd2f7365d1124c8450d3b66755399e4d221a7f
Artur Gręziak <alex4448@wp.pl>
2020-02-20 23:07:24 +0100

    Hello Say-if

---
+++ 73.Say-Hello-Say-Bye.js
@@ -0,0 +1,17 @@
+
+
+function sayHelloBye(name, num) {
+
+if(num === 1){
+    return "Hello " + name[0].toUpperCase() + name.toLowerCase().slice(1);;
+} else if(num === 0){
+    return "Bye " + name[0].toUpperCase() + name.toLowerCase().slice(1);;
+}
+}
+console.log(sayHelloBye("artur", 1));
+
+console.log(sayHelloBye("alon", 1)); //➞ "Hello Alon"
+
+console.log(sayHelloBye("Tomi", 0)); // ➞ "Bye Tomi"
+
+console.log(sayHelloBye("jose", 0)); // ➞ "Bye Jose"
